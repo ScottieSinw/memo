288706ef1ee2539a907f58d08dcd2eb43a71f722
Simwoo Lee <simwoo@example.com>
2025-07-11 10:45:17 +0200

    Downladed react-boilerplate-cra-template

---
+++ my-app/src/styles/__tests__/GlobalStyleWrapper.tsx
@@ -0,0 +1,7 @@
+// src/styles/GlobalStyleWrapper.tsx
+import React from 'react';
+import { GlobalStyle } from '../global-styles';
+
+export const GlobalStyleWrapper: React.FC = () => {
+  return <GlobalStyle />;
+};
